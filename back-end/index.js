4c32b30a1fcb66e90eeba99a91aa059c740cc0e2
Manan Patel <202201310@daiict.ac.in>
2024-10-30 18:32:36 +0530

    Update index.js
    Added the functionality of Google Sign Up

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,26 @@
+require('dotenv').config(); //Manan
+
 const express = require("express");
 const {connectToMongoDB} = require("./connection");
 const userRoute = require("./server/routes/user");
+const session = require('express-session'); //Manan
 
 const app = express();
 const PORT = 8000;
 
-connectToMongoDB("mongodb://localhost:27017/user").then(() => {
+connectToMongoDB("mongodb://localhost:27017/AskAtEase").then(() => {
     console.log("MongoDB connected successfully!!");
 });
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET 
+})); //Manan
+
+app.set('view engine', 'ejs'); //Manan 
+const userRoutes = require('./routes/userRoute');//Manan
+app.use('/',userRoutes); //Manan
 
 app.use(express.json());
 
