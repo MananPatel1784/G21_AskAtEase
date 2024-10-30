46d4f3cbda3b04b45365ad03cf89424ce6e67ec5
Manan Patel <202201310@daiict.ac.in>
2024-10-30 18:43:48 +0530

    Create userRoute.js

---
+++ back-end/server/routes/userRoute.js
@@ -0,0 +1,33 @@
+//Entire file by Manan
+
+const express = require('express');
+const router = express();
+const passport = require('passport'); 
+require('./passport');
+
+router.use(passport.initialize()); 
+router.use(passport.session());
+
+const userController = require('../controllers/userController');
+
+router.get('/', userController.loadAuth);
+
+// Auth 
+router.get('/auth/google' , passport.authenticate('google', { scope: 
+	[ 'email', 'profile' ] 
+})); 
+
+// Auth Callback 
+router.get( '/auth/google/callback', 
+	passport.authenticate( 'google', { 
+		successRedirect: '/success', 
+		failureRedirect: '/failure'
+}));
+
+// Success 
+router.get('/success' , userController.successGoogleLogin); 
+
+// failure 
+router.get('/failure' , userController.failureGoogleLogin);
+
+module.exports = router;
