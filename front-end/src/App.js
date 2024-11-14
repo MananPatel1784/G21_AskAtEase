5700622b3a038ffe6b24268d72c3e6035485656d
Dharmik <dharmikgodhani0@gmail.com>
2024-11-14 17:41:43 +0530

    Created SideBarOptions and SideBar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import Login from './Components/LoginSignUp/Login';
-import SignUp from './Components/LoginSignUp/SignUp'; // It's fine to import but not necessary unless you use it
-import { AuthProvider } from './contexts/AuthContext';
+import "./App.css";
+import Login from "./Components/LoginSignUp/Login";
+import SignUp from "./Components/LoginSignUp/SignUp"; // It's fine to import but not necessary unless you use it
+import { AuthProvider } from "./contexts/AuthContext";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
-    <div>
-      <SignUp />     
-    </div>
+    <>
+      <div>
+        <Sidebar />
+      </div>
+    </>
   );
 }
 
