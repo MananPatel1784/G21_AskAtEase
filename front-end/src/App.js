7c9bbaa782ed81a6829af00d2c01110de63150fb
KhushiV574 <202201332@daiict.ac.in>
2024-11-14 12:18:13 +0530

    app.js updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,17 +3,21 @@ import QuoraBox from './QuoraBox';  // Import QuoraBox component
 import { Provider } from 'react-redux'; // Import Provider from react-redux
 import store from './store'; // Correct path for store.js
 import Login from './Components/LoginSignUp/Login';
+import SignUp from './Components/LoginSignUp/SignUp';
+import { AuthProvider } from './contexts/AuthContext'; // Make sure this is imported
 
 function App() {
   return (
-    <>
-      <Login /> {/* Render Login component */}
+    <AuthProvider> {/* Wrap entire app or specific parts where authentication is needed */}
       <Provider store={store}>  {/* Wrap app with Provider to access Redux store */}
         <div>
+          {/* Example conditionally rendering Login, QuoraBox, and SignUp */}
+          <Login /> {/* Render Login component */}
           <QuoraBox />  {/* QuoraBox will now have access to Redux store */}
+          <SignUp /> {/* Render SignUp component */}
         </div>
       </Provider>
-    </>
+    </AuthProvider>
   );
 }
 
