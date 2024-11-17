29cfff8f8ce45fd02141408d85b4b5cc62972bcd
Jayesh <jayeshdpadiya1802@gmail.com>
2024-11-17 15:37:17 +0530

    solved errors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-// import Login from "./LoginSignup//LoginSignUp/Login";
-// import SignUp from "../src/Components/LoginSignUp/SignUp";
+
+import SignUp from "../src/Components/LoginSignUp/SignUp";
 import { AuthProvider } from "./contexts/AuthContext";
 import Leftpart from "./Components/mainleftpart";
 import Gemini from "./Components/Gemini";
-import Questionsforyou from "./Components/Questionsforyou";
 import ProfilePage from "./Components/ProfilePage";
 import AddPost from "./Components/AddPost";
 import MainComponent from "./Components/maincomponent";
@@ -12,35 +11,37 @@ import Header from "./Components/MainHeader";
 import QuestionSidebar from "./Components/QuestionsSidebar"
 import AnswerPage from "./Components/Answerpage";
 import { BrowserRouter , Routes , Route } from "react-router-dom";
+import QuestionsAndAnswers from "./Components/QuestionAnswer";
+import Login from "./Components/LoginSignUp/Login";
 // import WidgetContent from './Components/WidgetContent';
 
 
 function App() {
   return (
     <>
+    
       <div>
         {/* <Login />  */}
         {/* <SignUp /> */}
         {/* <Sidebar /> */}
           
         {/* <ProfilePage/> */}
-        <MainComponent/>
-        <AnswerPage/>
+        {/* <MainComponent/> */}
+        {/* <AnswerPage/> */}
         {/* <QuestionSidebar/> */}
         {/* <Leftpart/>  */}
         {/* <Login/>  */}
         {/* <SignUp />  */}
-        <Questionsforyou />
-        {/* <Routes>
+        {/* <Questionsforyou /> */}
+         <Routes>
               <Route path="/" element={<MainComponent />}/>
               <Route path="Answer" element={<AnswerPage/>}/> 
               <Route path="ReturnHome" element={<MainComponent/>}/>
-              {/* <Route path="/signup" element={<SignUp />} />
-              <Route path ="/login" element={<Login/>}/> */}
-              {/* <Route path="/gemini" element={<Gemini />} /> 
-
-              
-        </Routes> */} 
+               <Route path="/signup" element={<SignUp />} />
+              <Route path ="/login" element={<Login/>}/> 
+               <Route path="/gemini" element={<Gemini />} /> 
+               <Route path="questions-and-answers" element={<QuestionsAndAnswers/>}/>
+          </Routes>  
         </div>
     </>
   );
